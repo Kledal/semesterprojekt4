db42d08ec4601af3498e786f17733f4281f7e3db
Marcus Frederiksen <201371033@iha.dk>
2015-04-22 15:20:43 +0200

    Added printer commands

---
+++ Makerbotv2/Makerlab/Migrations/201504221157305_AddedPrinterCommand1.cs
@@ -0,0 +1,31 @@
+namespace Makerlab.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedPrinterCommand1 : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.PrinterCommand",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Command = c.String(),
+                        Printer_Id = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Printer", t => t.Printer_Id)
+                .Index(t => t.Printer_Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.PrinterCommand", "Printer_Id", "dbo.Printer");
+            DropIndex("dbo.PrinterCommand", new[] { "Printer_Id" });
+            DropTable("dbo.PrinterCommand");
+        }
+    }
+}
---
+++ Makerbotv2/Makerlab/Migrations/201504221158552_AddedPrinterCommand2.cs
@@ -0,0 +1,16 @@
+namespace Makerlab.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedPrinterCommand2 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
