a043b59fe2998b350af1a3e4c8a491d74e786986
Ranth67 <Ralle_08@hotmail.com>
2015-04-23 13:49:18 +0200

    Lavet selectbox og dropdown menu for printerer

---
+++ Makerbotv2/Makerlab/DAL/PrinterManager.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Makerlab.Models;
+
+namespace Makerlab.DAL
+{
+    public class PrinterManager
+    {
+        public static Printer Create(Printer printer)
+        {
+            using (var db = new MakerContext())
+            {
+                db.Printers.Add(printer);
+                db.SaveChanges();
+
+                return printer;
+            }
+        }
+
+        public static Printer Update(Printer printer)
+        {
+            using (var db = new MakerContext())
+            {
+                db.Printers.Attach(printer);
+
+                var updatedPrinter = db.Entry(printer);
+                updatedPrinter.State = EntityState.Modified;
+
+                db.SaveChanges();
+
+                return updatedPrinter.Entity;
+            }
+        }
+
+        public static IEnumerable<Printer> Read()
+        {
+            using (var db = new MakerContext())
+            {
+                var printers = db.Printers
+                    .Include(u => u.Bookings).ToList();
+                return printers;
+            }
+        }
+
+        public static Printer Read(int id)
+        {
+            using (var db = new MakerContext())
+            {
+                var printer = db.Printers.Include(u => u.Bookings)
+                    .Include(u => u.Bookings).SingleOrDefault(u => u.Id == id);
+
+                return printer;
+            }
+        }
+
+        public static void Delete(Printer printer)
+        {
+            using (var db = new MakerContext())
+            {
+                db.Printers.Attach(printer);
+                db.Printers.Remove(printer);
+                db.SaveChanges();
+            }
+        }
+    }
+}
