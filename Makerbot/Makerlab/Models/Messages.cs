ae897e61863d4acea44627b3fbbc034617e8264e
Kåre Jensen <201370886@iha.dk>
2015-05-07 15:35:03 +0200

    Added messages to database

--- Makerbot/Makerlab/Models/Messages.cs
+++ Makerbot/Makerlab/Models/Messages.cs
@@ -7,6 +7,7 @@ namespace Makerlab.Models
 {
     public class Messages
     {
+        public int Id { get; set; }
         public string Message { get; set; }
 
         public string Type { get; set; }
