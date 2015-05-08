e0a70c9a743297b26fc73981371fe759b3b1d1d6
Kåre Jensen <201370886@iha.dk>
2015-05-08 12:56:46 +0200

    Fixed message

---
+++ Makerbot/Makerlab/Migrations/201505081054379_FixedMessage.cs
@@ -0,0 +1,38 @@
+namespace Makerlab.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class FixedMessage : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Message",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Content = c.String(),
+                        Type = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            DropTable("dbo.Messages");
+        }
+        
+        public override void Down()
+        {
+            CreateTable(
+                "dbo.Messages",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Message = c.String(),
+                        Type = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            DropTable("dbo.Message");
+        }
+    }
+}
