fcb3ba896a20077a1e975a3c3237b535d3b9aadb
Christian <201370942@post.au.dk>
2015-05-14 18:26:08 +0200

    added flere tests

---
+++ Makerbot/Database/Makerlabr.Tests/Controllers/FrontendControllerTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Web.Mvc;
+using Makerlab.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace Makerlabr.Tests.Controllers
+{
+    [TestFixture]
+    public class FrontendControllerTest
+    {
+        private FrontendController _uut;
+
+        [SetUp]
+        public void SetUp()
+        {
+           _uut = new FrontendController();
+        }
+        
+        [Test]
+        public void Logind_Returns_View()
+        {
+            string viewName = "";
+            Assert.AreEqual(viewName, ((ViewResult)_uut.Index()).ViewName, "View name should have been {0}", viewName);
+        }
+    }
+}
