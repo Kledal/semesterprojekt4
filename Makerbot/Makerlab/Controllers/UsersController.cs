4f0b812be81835bcaf434fd8b29e34fcdc520fce
Kåre Jensen <201370886@iha.dk>
2015-05-14 14:26:09 +0200

    Added UserController and Not-Approved users table in dashboard/users

---
+++ Makerbot/Makerlab/Controllers/UsersController.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using Makerlab.Extensions;
+using Makerlab.Models;
+
+namespace Makerlab.Controllers
+{
+    [Auth("Administrator")]
+    public class UsersController : ApplicationController
+    {
+        private MakerContext db = new MakerContext();
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: /Userroles/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,FirstName,LastName,Email,StudieNummer,AccessCard,UserRoldId")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(user).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Users", "Dashboard");
+            }
+            return View(user);
+        }
+
+        // GET: /Userroles/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: /Userroles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            User user = db.Users.Find(id);
+            db.Users.Remove(user);
+            db.SaveChanges();
+            return RedirectToAction("Users", "Dashboard");
+        }
+
+        // GET: /Printers/Delete/5
+        public ActionResult Approve(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: /Messages/Delete/5
+        [HttpPost, ActionName("Approve")]
+        public ActionResult ApproveConfirmed(int id)
+        {
+            User user = db.Users.Find(id);
+            user.UserRoleId = 2;
+            db.SaveChanges();
+            return RedirectToAction("Users", "Dashboard");
+        }
+    }
+}
--- Makerbot/Makerlab/Migrations/Configuration.cs
+++ Makerbot/Makerlab/Migrations/Configuration.cs
@@ -24,7 +24,26 @@ namespace Makerlab.Migrations
                 new UserRole { Id = 2, CanCreateBooking = true, CanViewDashboard = false, RoleName = "ApprovedUser" },
                 new UserRole { Id = 3, CanCreateBooking = false, CanViewDashboard = false, RoleName = "DefaultUser" });
             context.Users.AddOrUpdate(u => u.Id,
-                new User() { Id = 1, FirstName = "Test", LastName = "Person", AccessCard = 12345678, Email = "jj@testidp.wayf.dk", StudieNummer = 123456789, UserRoleId = 1 });
+                new User()
+                {
+                    Id = 1, 
+                    FirstName = "Test", 
+                    LastName = "Person", 
+                    AccessCard = 12345678, 
+                    Email = "jj@testidp.wayf.dk", 
+                    StudieNummer = 123456789, 
+                    UserRoleId = 1
+                },
+                new User()
+                {
+                    Id = 2,
+                    FirstName = "Ikke",
+                    LastName = "Godkendt",
+                    AccessCard = 123,
+                    Email = "test@test.dk",
+                    StudieNummer = 321,
+                    UserRoleId = 3
+                });
             context.Printers.AddOrUpdate(p => p.Id,
                   new Printer() { Id = 1, Name = "Gandalf", IsBookable = true, LastSeen = DateTime.Now },
                   new Printer() { Id = 2, Name = "Frodo", IsBookable = true, LastSeen = DateTime.Now },
@@ -34,8 +53,15 @@ namespace Makerlab.Migrations
                 new File() { Id = 1, FileName = "Testfile1" },
                 new File() { Id = 2, FileName = "Testfile2" });
             context.Bookings.AddOrUpdate(x => x.Id,
-                new Booking() { Id = 1, PrinterId = 1, UserId = 1, FileId = 1, StartTime = new DateTime(2015, 5, 5, 8, 30, 00), EndTime = new DateTime(2015, 5, 5, 19, 00, 00) }
-                );
+                new Booking()
+                {
+                    Id = 1, 
+                    PrinterId = 1, 
+                    UserId = 1, 
+                    FileId = 1, 
+                    StartTime = new DateTime(2015, 5, 14, 8, 30, 00), 
+                    EndTime = new DateTime(2015, 5, 14, 19, 00, 00)
+                });
         }
     }
 }
