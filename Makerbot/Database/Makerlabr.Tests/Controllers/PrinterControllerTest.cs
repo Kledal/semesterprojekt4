4b767fa2b58f464e89f141000451bbc49102dc81
Kledal <alex.kledal@gmail.com>
2015-05-19 10:06:53 +0200

    Fixed details printer test

--- Makerbot/Database/Makerlabr.Tests/Controllers/PrinterControllerTest.cs
+++ Makerbot/Database/Makerlabr.Tests/Controllers/PrinterControllerTest.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Web.Configuration;
 using System.Web.Mvc;
+using System.Xml.XPath;
 using Makerlab.Controllers;
 using Makerlab.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -20,21 +21,21 @@ namespace Makerlabr.Tests.Controllers
         {
             _uut = new PrintersController();
         }
-        
+
         [Test]
         public void IndexAction_Returns_View()
         {
-           
+
             string viewName = "Index";
             Assert.AreEqual(viewName, ((ViewResult)_uut.Index()).ViewName, "View name should have been {0}", viewName);
 
         }
-        
+
         [Test]
         public void CreateAction_Returns_IndexViewWhenModelStateIsValid()
         {
             _uut.ModelState.Clear();
-           
+
             RedirectToRouteResult result = _uut.Create(new Printer()) as RedirectToRouteResult;
             result.RouteValues["Action"].Equals("Index");
             Assert.Equals("Action", result.RouteValues["Index"]);
@@ -43,8 +44,8 @@ namespace Makerlabr.Tests.Controllers
         [Test]
         public void CreateAction_Adds_PrinterToDBWhenModelStateIsValid()
         {
-           
-            ActionResult result = _uut.Create(new Printer()) as ActionResult; 
+
+            ActionResult result = _uut.Create(new Printer()) as ActionResult;
         }
 
         [Test]
@@ -62,7 +63,7 @@ namespace Makerlabr.Tests.Controllers
         [Test]
         public void EditAction_Returns_ViewWhenModelstateIsInvalid()
         {
-         
+
             _uut.ModelState.Add("data", new ModelState());
             _uut.ModelState.AddModelError("testError", "test");  // Gør modelstate invalid. 
 
@@ -72,9 +73,9 @@ namespace Makerlabr.Tests.Controllers
         }
 
         [Test]
-        public void  DeleteAction_Returns_Printerview()
+        public void DeleteAction_Returns_Printerview()
         {
-       
+
             ActionResult result = _uut.Delete(2); //  uut modtager et ID.
             Assert.IsInstanceOfType(result, typeof(ViewResult));
         }
@@ -82,39 +83,36 @@ namespace Makerlabr.Tests.Controllers
         [Test]
         public void DeleteConfirmedAction_Returns_Printerview()
         {
-            
+
             RedirectToRouteResult result = _uut.Create(new Printer()) as RedirectToRouteResult;
             ActionResult aresult = _uut.DeleteConfirmed(3);
 
             Assert.AreEqual("Index", result.RouteValues["Index"]);
-          
+
         }
 
         [Test]
         public void DeleteConfirmedAction_Deletes_Printer()
         {
-            var john = true; 
-            Assert.Equals("false",john);
+            var john = true;
+            Assert.Equals("false", john);
         }
 
         [Test]
         public void Details_Returns_HttpBadRequestWhenIdIsNull()
         {
-            ActionResult  result = _uut.Details(null);
-
+            ActionResult result = _uut.Details(null);
             Assert.IsInstanceOfType(result, typeof(HttpStatusCodeResult));
         }
-
 
         [Test]
         public void Details_Returns_HttpNotFoundWhenPrinterIsNull()
         {
 
-           var printer = new Printer();
-           printer.Id = 0;          
-           ActionResult result = _uut.Details(2);
-
-           Assert.AreEqual("HttpNotFound",result);
+            var printer = new Printer();
+            printer.Id = 0;
+            ActionResult result = _uut.Details(printer.Id);
+            Assert.IsInstanceOfType(result, typeof(HttpNotFoundResult));
         }
 
     }
